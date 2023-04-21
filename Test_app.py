b5a37f8b08f6bd252a0775c2ea065fbadd8e5ba2
Seamus1992 <spbartholdy@gmail.com>
2023-04-21 08:50:18 +0200

    Update Test_app.py

--- Test_app.py
+++ Test_app.py
@@ -63,10 +63,9 @@ def Wellness_data():
         elif 'Hvor udmattet er du?' in row and row['Hvor udmattet er du?'] == 5 or 'Bedøm din muskelømhed' in row and row['Bedøm din muskelømhed'] == 5 or 'Hvordan har du det mentalt?' in row and row['Hvordan har du det mentalt?'] == 5 or ('Jeg følte mig tilpas udfordret under træning/kamp' in row and row['Jeg følte mig tilpas udfordret under træning/kamp'] == 5) or ('Min tidsfornemmelse forsvandt under træning/kamp' in row and row['Min tidsfornemmelse forsvandt under træning/kamp'] == 5 or 'Jeg oplevede at tanker og handlinger var rettet mod træning/kamp' in row and row['Jeg oplevede at tanker og handlinger var rettet mod træning/kamp'] == 5):
             color = 'yellow'
         return ['background-color: %s' % color] * row.size
-    eftertræning.set_index('Tidsstempel', inplace=True)
-    eftertræning.sort_index(ascending=False, inplace=True)
+    #eftertræning.set_index('Tidsstempel', inplace=True)
+    #eftertræning.sort_index(ascending=False, inplace=True)
     eftertræning = eftertræning.astype(int,errors='ignore')
-    eftertræning = eftertræning.reset_index(drop=True)
     eftertræning = eftertræning.style.apply(color_row, axis=1, subset=pd.IndexSlice[:])
 
 
