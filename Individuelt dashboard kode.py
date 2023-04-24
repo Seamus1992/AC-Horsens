a9b66f79f2529163e0be298eada41938016f2fce
Seamus1992 <spbartholdy@gmail.com>
2023-04-24 14:25:37 +0200

    Update Individuelt dashboard kode.py

--- Individuelt dashboard kode.py
+++ Individuelt dashboard kode.py
@@ -5,15 +5,10 @@ df.rename(columns={'playerId': 'Player id'}, inplace=True)
 df = df.astype(str)
 dfevents = pd.read_csv(r'C:\Users\SéamusPeareBartholdy\Documents\GitHub\AC-Horsens\U15 eventdata.csv')
 dfevents = dfevents[['Player id','Player name','label','date','matchId']]
-#dfevents['Player id'] = dfevents['Player id'].drop_duplicates(keep='first')
 dfspillernavn = df[['Player id','matchId','positions','total','average','percent']]
 dfspillernavn = dfspillernavn.astype(str)
-#dfspillernavn['matchId'] = dfspillernavn['matchId'].drop_duplicates(keep='first')
-#dfspillernavn = dfspillernavn.set_index('Player id')
 dfevents['Player id'] = dfevents['Player id'].astype(str)
 dfevents['matchId'] = dfevents['matchId'].astype(str)
-#dfevents['matchId'] = dfevents['matchId'].drop_duplicates(keep='first')
-#dfevents = dfevents.set_index('Player id')
 df = dfspillernavn.merge(dfevents)
 df['Player&matchId'] = df['Player id'] + df['matchId']
 df['Player&matchId'] = df['Player&matchId'].drop_duplicates(keep='first')
