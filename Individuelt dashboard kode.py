3258c2a20879fe35834cebae849d8f55c6921be3
Seamus1992 <spbartholdy@gmail.com>
2023-04-24 13:03:34 +0200

    Opdatering

--- Individuelt dashboard kode.py
+++ Individuelt dashboard kode.py
@@ -2,10 +2,23 @@ import pandas as pd
 import streamlit as st
 df = pd.read_csv(r'C:\Users\SéamusPeareBartholdy\Documents\GitHub\AC-Horsens\Individuelt dashboard test.csv')
 df.rename(columns={'playerId': 'Player id'}, inplace=True)
-dfevents = pd.read_csv(r'C:\Users\SéamusPeareBartholdy\Documents\GitHub\AC-Horsens\U15 eventdata.csv')
-dfevents = dfevents[['Player id','Player name']]
 df = df.astype(str)
-df = pd.concat([df,dfevents])
-df = df[['Player id','Player name','matchId','positions','total','average','percent']]
+dfevents = pd.read_csv(r'C:\Users\SéamusPeareBartholdy\Documents\GitHub\AC-Horsens\U15 eventdata.csv')
+dfevents = dfevents[['Player id','Player name','label','date','matchId']]
+#dfevents['Player id'] = dfevents['Player id'].drop_duplicates(keep='first')
+dfspillernavn = df[['Player id','matchId','positions','total','average','percent']]
+dfspillernavn = dfspillernavn.astype(str)
+#dfspillernavn['matchId'] = dfspillernavn['matchId'].drop_duplicates(keep='first')
+#dfspillernavn = dfspillernavn.set_index('Player id')
+dfevents['Player id'] = dfevents['Player id'].astype(str)
+dfevents['matchId'] = dfevents['matchId'].astype(str)
+#dfevents['matchId'] = dfevents['matchId'].drop_duplicates(keep='first')
+#dfevents = dfevents.set_index('Player id')
+df = dfspillernavn.merge(dfevents)
+df['Player&matchId'] = df['Player id'] + df['matchId']
+df['Player&matchId'] = df['Player&matchId'].drop_duplicates(keep='first')
+df = df.dropna()
+df = df[['Player id','Player name','matchId','label','date','positions','total','average','percent']]
+df = df.set_index('Player id')
 st.dataframe(df)
 print(df)
--- Wellness.py
+++ Wellness.py
@@ -9,3 +9,46 @@ ws = sh.worksheet('Samlet')
 df = pd.DataFrame(ws.get_all_records())
 df.to_csv('ny wellness.csv',index=False)
 print('Wellness data hentet')
+
+import pandas as pd
+import streamlit as st
+import seaborn as sns
+import os
+import glob
+import matplotlib.pyplot as plt
+import openpyxl as xlsxwriter
+from pandas import DataFrame
+os.chdir(r'C:\Users\SéamusPeareBartholdy\OneDrive - AC Horsens A S\Akademi\Excel Organisering og indhold af træning framework\GPS udtræk')
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
+df = pd.DataFrame(combined_csv)
+df = df.dropna()
+print ('GPS csv filer kombineret')
+df2 = pd.read_excel(r'C:\Users\SéamusPeareBartholdy\Documents\GitHub\AC-Horsens\GPS spillere.xlsx')
+dforiginal = df.merge(df2)
+os.chdir(r'C:\Users\SéamusPeareBartholdy\Documents\GitHub\AC-Horsens')
+dforiginal.to_excel(r'C:\Users\SéamusPeareBartholdy\Documents\GitHub\AC-Horsens\samlet gps data.xlsx', index=False)
+dforiginal = pd.read_excel(r'C:\Users\SéamusPeareBartholdy\Documents\GitHub\AC-Horsens\samlet gps data.xlsx')
+writer = pd.ExcelWriter(r'C:\Users\SéamusPeareBartholdy\Documents\GitHub\AC-Horsens\samlet gps data.xlsx', engine='xlsxwriter')
+dforiginal.to_excel(writer,sheet_name='Sheet1', index=None, header=True)
+
+
+workbook  = writer.book
+worksheet = writer.sheets['Sheet1']
+
+formatdict = {'num_format':'dd-mm-yyyy'}
+fmt = workbook.add_format(formatdict)
+worksheet.set_column('A:A', None, fmt)
+
+formatdict = {'num_format':'hh:mm:ss'}
+fmt = workbook.add_format(formatdict)
+worksheet.set_column('F:G', None, fmt)
+
+writer.close()
+dforiginal = pd.read_excel(r'C:\Users\SéamusPeareBartholdy\Documents\GitHub\AC-Horsens\samlet gps data.xlsx',decimal=',')
+Ugenummer = dforiginal['Date'].apply(lambda x: x.isocalendar()[1])
+dforiginal.insert(loc = 48, column = 'Ugenummer', value= Ugenummer)
+dforiginal.to_csv(r'C:\Users\SéamusPeareBartholdy\Documents\GitHub\AC-Horsens\samlet gps data.csv', index=False)
+os.remove(r'C:\Users\SéamusPeareBartholdy\Documents\GitHub\AC-Horsens\samlet gps data.xlsx')
+print('GPS færdig')
