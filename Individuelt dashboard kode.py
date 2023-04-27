439c358939ae3201c4992102ccf76ae68e7ccb18
Seamus1992 <spbartholdy@gmail.com>
2023-04-27 11:23:08 +0200

    Opdatering

--- Individuelt dashboard kode.py
+++ Individuelt dashboard kode.py
@@ -3,7 +3,7 @@ import streamlit as st
 df = pd.read_csv(r'C:\Users\SéamusPeareBartholdy\Documents\GitHub\AC-Horsens\Individuelt dashboard test.csv')
 df.rename(columns={'playerId': 'Player id'}, inplace=True)
 df = df.astype(str)
-dfevents = pd.read_csv(r'C:\Users\SéamusPeareBartholdy\Documents\GitHub\AC-Horsens\U15 eventdata.csv')
+dfevents = pd.read_csv(r'C:\Users\SéamusPeareBartholdy\Documents\GitHub\AC-Horsens\U15 eventdata.csv',low_memory=False)
 dfevents = dfevents[['Player id','Player name','label','date','matchId']]
 dfspillernavn = df[['Player id','matchId','positions','total','average','percent']]
 dfspillernavn = dfspillernavn.astype(str)
